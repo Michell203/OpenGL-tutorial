440843fe48ab6be491a2cf85b357596b2fe9713e
unknown <michaelhijazeen203@gmail.com>
2026-01-12 15:17:42 +0300

    Hello triangle exercises

--- main.cpp
+++ main.cpp
@@ -22,6 +22,14 @@ const char* fragmentShaderSource =
 "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
 "}\0";
 
+const char* fragmentShaderSource_yellow =
+"#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"   FragColor = vec4(1.0f, 1.0f, 0.0f, 1.0f);\n"
+"}\0";
+
 int main()
 {
     glfwInit();
@@ -52,28 +60,64 @@ int main()
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
 
-
-
     // Hello Triangle Chapter
     float vertices[] = {
      -0.5f, -0.5f, 0.0f,
      0.5f, -0.5f, 0.0f,
      0.0f,  0.5f, 0.0f
     };
+    
+    // Intro to Element Buffer Objects (EBO)
+    float rect_vertices[] = {
+         0.5f,  0.5f, 0.0f,  // top right
+         0.5f, -0.5f, 0.0f,  // bottom right
+        -0.5f, -0.5f, 0.0f,  // bottom left
+        -0.5f,  0.5f, 0.0f   // top left 
+    };
+    unsigned int indices[] = { // Specifiy order to draw triangles
+        0, 1, 3,     // first triangle
+        1, 2, 3      // second triangle
+    };
 
+    
+    /*float vertices_2_triangles[] = {
+     -0.2f, -0.2f, 0.0f,
+     0.2f, -0.2f, 0.0f,
+     0.0f,  0.2f, 0.0f,
+
+     -0.2f, -0.4f, 0.0f,
+     0.2f, -0.4f, 0.0f,
+     0.0f,  -0.6f, 0.0f,
+    };*/
+    
+    float vertices_1[] = {
+     -0.2f, -0.2f, 0.0f,
+     0.2f, -0.2f, 0.0f,
+     0.0f,  0.2f, 0.0f, 
+    };
+
+     float vertices_2[] = {
+     -0.2f, -0.4f, 0.0f,
+     0.2f, -0.4f, 0.0f,
+     0.0f,  -0.6f, 0.0f, 
+    };   
+    
+    unsigned int indices_2_tri[] = { // Specifiy order to draw triangles
+        0, 1, 2,     // first triangle
+        3, 4, 5      // second triangle
+    };
+
+    
     /** 
     Defined vertex data, would like to send it as input to the GPU
     Create memory in GPU to store this data and configure how OpenGL should interpret it
     We use Vertex Buffer Objects (VBO) to do that
     **/
-    
-    unsigned int VBO;
-    glGenBuffers(1, &VBO);
 
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    //glBindBuffer(GL_ARRAY_BUFFER, VBO);
 
     // Copy previosuly defined vertex data into the buffer
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    //glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
     // Basic vertex shader
     unsigned int vertexShader;
@@ -95,7 +139,7 @@ int main()
     fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
     glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
     glCompileShader(fragmentShader);
-
+    
     glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
     if (!success)
     {
@@ -103,24 +147,50 @@ int main()
         std::cout << "ERROR SHADER FRAGMENT COMPILE FAILED" << infoLog << std::endl;
     }
 
+    unsigned int fragmentShader_yellow;
+    fragmentShader_yellow = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader_yellow, 1, &fragmentShaderSource_yellow, NULL);
+    glCompileShader(fragmentShader_yellow);
+
+    glGetShaderiv(fragmentShader_yellow, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(fragmentShader_yellow, 512, NULL, infoLog);
+        std::cout << "ERROR SHADER FRAGMENT COMPILE FAILED" << infoLog << std::endl;
+    }
+
     // Shader progra
-    unsigned int shaderProgram;
+    unsigned int shaderProgram, shaderProgram_yellow;
     shaderProgram = glCreateProgram();
+    shaderProgram_yellow = glCreateProgram();
 
     glAttachShader(shaderProgram, vertexShader);
+    //glAttachShader(shaderProgram, vertexShader_2);
     glAttachShader(shaderProgram, fragmentShader);
+
+
+    glAttachShader(shaderProgram_yellow, vertexShader);
+    glAttachShader(shaderProgram_yellow, fragmentShader_yellow);
+
     glLinkProgram(shaderProgram);
     if (!success)
     {
-        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+        glGetShaderInfoLog(shaderProgram, 512, NULL, infoLog);
         std::cout << "ERROR SHADER COMPILE FAILED" << infoLog << std::endl;
     }
 
+    glLinkProgram(shaderProgram_yellow);
+    if (!success)
+    {
+        glGetShaderInfoLog(shaderProgram_yellow, 512, NULL, infoLog);
+        std::cout << "ERROR SHADER COMPILE FAILED" << infoLog << std::endl;
+    }
 
 
     // Delet shader objects after linking
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
+    glDeleteShader(fragmentShader_yellow);
 
     // Now we link vertex attribute to each vertex
     //glBindBuffer(GL_ARRAY_BUFFER, VBO);
@@ -142,37 +212,65 @@ int main()
     // Doesn't scale well, need to store state configs to an object and simply bind it to restore its state
     // We do this with a Vertex Array Object (VAO)
     
-    unsigned int VAO;
-    glGenVertexArrays(1, &VAO);
+    unsigned int VAO[2];
+    glGenVertexArrays(2, VAO);
 
+    unsigned int VBO[2];
+    glGenBuffers(2, VBO);
+
+   /* unsigned int VAO, VAO_2;
+    glGenVertexArrays(1, &VAO);
+    glGenVertexArrays(1, &VAO_2);
+
+    unsigned int VBO, VBO_2;
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &VBO_2);*/
+       
     // Bind vertex array object
-    glBindVertexArray(VAO);
+    glBindVertexArray(VAO[0]);
     // Copy vertex array in a buffer
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO[0]);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices_1), vertices_1, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);   
+
+    glBindVertexArray(VAO[1]);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO[1]);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices_2), vertices_2, GL_STATIC_DRAW);
+
+    //unsigned int EBO;
+    //glGenBuffers(1, &EBO);
+    //glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    //glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices_2_tri), indices_2_tri, GL_STATIC_DRAW);
+
     // Set vertex attribute pointers
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
-    
-    
-	// Keep window open until closed by user
+   
+    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE); // Draw as lines
 
+	// Keep window open until closed by user
     while (!glfwWindowShouldClose(window)) {
         processInput(window);
 
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
-
+        
         // Draw the object
         glUseProgram(shaderProgram);
-        glBindVertexArray(VAO);
+        //glBindVertexArray(VAO);
+        glBindVertexArray(VAO[0]);
         glDrawArrays(GL_TRIANGLES, 0, 3);
+        
+        glUseProgram(shaderProgram_yellow);
+        glBindVertexArray(VAO[1]);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        //glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        //glBindVertexArray(0);
 
 		glfwSwapBuffers(window);
         glfwPollEvents();
     }
-
-
 
     glfwTerminate();
     return 0;
