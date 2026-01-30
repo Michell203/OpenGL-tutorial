a19e767e2e7387dc6f656ab8d9edc7a6c1d091ad
unknown <michaelhijazeen203@gmail.com>
2026-01-30 12:15:48 +0300

    Added colour attributes to vertex array

--- main.cpp
+++ main.cpp
@@ -9,25 +9,32 @@ void processInput(GLFWwindow* window);
 const char* vertexShaderSource =
 "#version 330 core\n"
 "layout (location = 0) in vec3 aPos;\n"
+"layout (location = 1) in vec3 aColor;\n"
+"out vec4 vertexColor;\n" // Specify color output to the fragment shader
+"out vec3 ourColor2;\n"
 "void main()\n"
 "{\n"
-"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"   gl_Position = vec4(aPos, 1.0);\n"
+"   vertexColor = vec4(0.5, 0.0, 0.0, 0.1);\n" // Output variable is a dark red color
+"   ourColor2 = aColor;\n"
 "}\0";
 
 const char* fragmentShaderSource =
 "#version 330 core\n"
 "out vec4 FragColor;\n"
+"uniform vec4 ourColor;\n"
 "void main()\n"
 "{\n"
-"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"   FragColor = ourColor;\n"
 "}\0";
 
 const char* fragmentShaderSource_yellow =
 "#version 330 core\n"
 "out vec4 FragColor;\n"
+"in vec3 ourColor2;\n"
 "void main()\n"
 "{\n"
-"   FragColor = vec4(1.0f, 1.0f, 0.0f, 1.0f);\n"
+"   FragColor = vec4(ourColor2, 1.0f);\n"
 "}\0";
 
 int main()
@@ -58,7 +65,7 @@ int main()
 
     glViewport(0, 0, 800, 600);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-
+    
 
     // Hello Triangle Chapter
     float vertices[] = {
@@ -97,9 +104,9 @@ int main()
     };
 
      float vertices_2[] = {
-     -0.2f, -0.4f, 0.0f,
-     0.2f, -0.4f, 0.0f,
-     0.0f,  -0.6f, 0.0f, 
+     -0.2f, -0.4f, 0.0f,    1.0f, 0.0f, 0.0f, // Position,   Color
+     0.2f, -0.4f, 0.0f,     0.0f, 1.0f, 0.0f,
+     0.0f,  -0.6f, 0.0f,    0.0f, 0.0f, 1.0f,
     };   
     
     unsigned int indices_2_tri[] = { // Specifiy order to draw triangles
@@ -107,6 +114,9 @@ int main()
         3, 4, 5      // second triangle
     };
 
+    int nrAttributes;
+    glGetIntegerv(GL_MAX_VERTEX_ATTRIBS, &nrAttributes);
+    std::cout << "Maximum nr of vertex attributes supported: " << nrAttributes << std::endl;
     
     /** 
     Defined vertex data, would like to send it as input to the GPU
@@ -244,9 +254,14 @@ int main()
     //glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices_2_tri), indices_2_tri, GL_STATIC_DRAW);
 
     // Set vertex attribute pointers
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    // Position
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
+    // Color
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3* sizeof(float)));
+    glEnableVertexAttribArray(1);
    
+
     //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE); // Draw as lines
 
 	// Keep window open until closed by user
@@ -258,6 +273,14 @@ int main()
         
         // Draw the object
         glUseProgram(shaderProgram);
+        // Let's change colors
+        float timeValue = glfwGetTime();
+        float greenValue = (sin(timeValue / 2.0f) + 0.5f);
+        int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
+
+        if (vertexColorLocation == -1) { return 0; } // Could not find ourColor
+
+        glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
         //glBindVertexArray(VAO);
         glBindVertexArray(VAO[0]);
         glDrawArrays(GL_TRIANGLES, 0, 3);
